ec6b3077d0bc7d2a3fab6a3e4428f36c2d0e728d
vishnujmd <vishnujmd19@gmail.com>
2022-07-29 23:29:31 -0700

    1 st commit

---
+++ src/main/java/temp/com/App.java
@@ -0,0 +1,13 @@
+package temp.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
